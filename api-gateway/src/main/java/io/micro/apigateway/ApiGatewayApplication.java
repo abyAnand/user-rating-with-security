7ac79ab43e75ff7fc3abddc36feec6a584bdd988
abyAnand <abianand382@gmail.com>
2023-05-09 19:17:17 +0530

    Added User Service,Ratings Service, Hotel Service, Registry Server, API-GATEWAY

--- api-gateway/src/main/java/io/micro/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/io/micro/apigateway/ApiGatewayApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 public class ApiGatewayApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
